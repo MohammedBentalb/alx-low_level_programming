09c58321008a48f1e2e412034c8589fa62e8fd11
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-03-17 14:11:18 -0700

    Task : 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+
+i = 'a';
+
+while (i < 'g')
+{
+putchar(i);
+i++;
+}
+
+putchar('\n');
+return (0);
+}
