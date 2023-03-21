683dadadfd4a84c19de3e9d95c0fbf1a200a1b9f
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-03-21 15:15:29 -0700

    Task : 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+
+char j;
+int i;
+for(i = 0; i <=9; i++)
+{
+for(j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
