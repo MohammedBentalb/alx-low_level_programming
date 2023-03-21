d8bdfd30162b2821d653f307df8625149c501bec
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-03-21 15:20:01 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 #include "main.h"
 
+/**
+ *print_alphabet_x10 - Funtion to print 10 lines of alphabets
+ *
+ */
+
+
 void print_alphabet_x10(void)
 {
 
 char j;
 int i;
-for(i = 0; i <=9; i++)
+for (i = 0; i <= 9; i++)
 {
-for(j = 'a'; j <= 'z'; j++)
+for (j = 'a'; j <= 'z'; j++)
 {
 _putchar(j);
 }
