d7d6968f88a211368b10a9893f043a442ae5494f
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-03-27 14:56:51 -0700

    Task : 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+
+void puts2(char *str)
+{
+
+int i;
+for (i = 0; str[i] != '\0'; i += 2)
+{
+_putchar(str[i]);
+
+}
+}
