42f6ca530042a82635f0cf5a189db2230c455d1d
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-08 10:20:21 -0700

    Task : 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry function
+ * @argc: command line arguments's number
+ * @argv: array contains the program's command line arguments
+ * Return: Return 0 (success)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
