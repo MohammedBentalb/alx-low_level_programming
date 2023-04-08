8d7c5f2a9f7f3aadc53e9eaad7762f10131bea98
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-08 10:12:49 -0700

    Task : 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry function
+ * @argc: command line arguments's number
+ * @argv: array contains the program's command line arguments
+ * Return: Return 0 (success)
+ */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
