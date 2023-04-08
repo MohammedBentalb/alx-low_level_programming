c6f4b2ddde1357ad3f61aec866d8192accb6f098
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-08 10:43:47 -0700

    Task : 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry function
+ * @argc: command line arguments's number
+ * @argv: array contains the program's command line arguments
+ * Return: Return 0 (success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+	if (argc != 3)
+	{	printf("Error , print only two numbers\n");
+		return (1);
+	}
+
+
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+return (0);
+}
