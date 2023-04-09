dc88869fae6ca8f4e3f5ce884c34599571d9c696
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-09 08:52:55 -0700

    Task : 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry function
+ * @argc: The number of arguments supplied to the program
+ * @argv: array of pointers to the arguments
+ * Return: Return 1 || 0
+ */
+int main(int argc, char *argv[])
+{
+	int n, cents = 0;
+
+	n = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+while (n > 0)
+{
+	cents++;
+	if (n - 25 >= 0)
+	{
+	n -= 25;
+	continue;
+	}
+	if (n - 10 >= 0)
+	{
+	n -= 10;
+	continue;
+	}
+	if (n - 5 >= 0)
+	{
+	n -= 5;
+	continue;
+	}
+	if (n - 2 >= 0)
+	{
+	n -= 2;
+	continue;
+	}
+	if (n - 1 >= 0)
+	{
+	n -= 1;
+	continue;
+	}
+}
+	printf("%d\n", cents);
+return (0);
+}
