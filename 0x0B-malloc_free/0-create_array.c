6c2aa62735c7a6c3272c302af6e518b442ad8308
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-11 07:39:46 -0700

    Task : 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 	size_t i;
-	char *p = (int *) malloc(size * sizeof(char));
+	char *p = malloc(size * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
