93820bcaba07b5232ab9831460a4330b6a8843cc
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-13 18:40:51 -0700

    Task : 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,20 +14,20 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
-	if (old_size == new_size)
-		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{	free(ptr);
+		return (NULL);
+	}
 
 	if (ptr == NULL)
-	{
+        {
 		ptr = malloc(new_size);
 			if (ptr == NULL)
 				return (NULL);
 	}
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		return (NULL);
-	}
+	if (old_size == new_size)
+		return (ptr);
 
 	free(ptr);
 	ptr = malloc(new_size);
