6f89ba51a6445f30a45be98de54bbad55efa2638
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-13 18:50:59 -0700

    Task : 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,30 +7,49 @@
 * @ptr: pointer
 * @old_size: as the variable name's tell
 * @new_size: the name tells it all
-* Return: Return ptr || null
+* Return: Returnp p || ptr || null
 */
-
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-
-	if (new_size == 0 && ptr != NULL)
-	{	free(ptr);
-		return (NULL);
-	}
-
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-			if (ptr == NULL)
-				return (NULL);
-	}
+	unsigned int i, j;
+	char *p;
 
 	if (old_size == new_size)
 		return (ptr);
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
 
-	free(ptr);
-	ptr = malloc(new_size);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = malloc(new_size * sizeof(char));
+			if (newPtr == NULL)
+				return (NULL);
 
-return (ptr);
+	if (old_size < new_size)
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			p[i] = ((char *)ptr)[i];
+		}
+		free(ptr);
+		return (p);
+	}
+	else
+	{
+		for (j = 0; j < new_size; j++)
+		{
+			p[j] = ((char *)ptr)[j];
+		}
+		free(ptr);
+		return (p);
+	}
 }
