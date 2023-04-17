1ab74d3fafa26935c51286d302a1d8ef835f5651
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-17 16:22:27 -0700

    Task : 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int i, j;
 	dog_t *p;
 
-
 	while (name[l1] != '\0')
 		l1++;
 	while (owner[l2] != '\0')
@@ -28,19 +27,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p);
 		return (NULL);
 	}
-
 	p->name = malloc(l1 * sizeof(char));
 	if (p->name == NULL)
 	{
 		free(p->name);
 		return (NULL);
 	}
-
 	for (i = 0; i < l1; i++)
 	{
 		p->name[i] = name[i];
 	}
-
 	p->age = age;
 	p->owner = malloc(l2 * sizeof(char));
 	if (p->owner == NULL)
@@ -50,7 +46,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p);
 		return (NULL);
 	}
-
 	for (j = 0; j < l2; j++)
 	{
 		p->owner[j] = owner[j];
