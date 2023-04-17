11421ced115f9a3fd067e706a17b2c8e2cb44422
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-17 14:36:34 -0700

    Task : 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -24,14 +24,10 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)\n");
 		}
-		if (d->age != 0)
-		{
+
 			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: (nil)\n");
-		}
+
+
 		if (d->owner != NULL)
 		{
 			printf("Owner: %s\n", d->owner);
