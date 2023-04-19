554a320f7a3a8cdb9b94aa623066ab97f443d1dc
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-19 14:32:17 -0700

    Task : 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,6 +5,7 @@
 /**
  * array_iterator - Fruntion that executes a function on each element of array
  * @array: array
+ * @size: sive of array
  * @action: Function pointer
  */
 
@@ -12,9 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if(array != NULL && size != 0 && action != NULL)
+	if (array != NULL && size != 0 && action != NULL)
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 			action(array[i]);
 	}
 }
