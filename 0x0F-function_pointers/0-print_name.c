ed885ad5735443734317279d2d684be22c5cbe6d
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-19 14:18:01 -0700

    Task : 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
 /**
  * print_name - Function that prints the name
  * @name: the name
- * @action: Function pointer
+ * @f: Function pointer
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
