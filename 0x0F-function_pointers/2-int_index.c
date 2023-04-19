d3ea3eaef5b2cb6e434ffa269fa7717b94fca6d7
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-19 14:50:33 -0700

    Task : 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that search for integer inside array
+ * @array: array
+ * @size: size of array
+ * @cmp: Function pointer
+ * return: Return i || -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
