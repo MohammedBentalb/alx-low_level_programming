dfc0afec4fbc04c2324eec8decb5094250cbdaa2
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-19 20:04:59 -0700

    Task : 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	char *p_to_main = (char *)main;
+	char *p_to_main;
 	int number;
 	int i;
 
@@ -29,15 +29,16 @@ if (number < 0)
 	exit(2);
 }
 
+	p_to_main = (char *)main;
 
-for (i = 0; i < number; i++)
-{
-if (i == number  - 1)
-{
-printf("%02hhx\n", p_to_main[i]);
-break;
-}
-printf("%02hhx ", p_to_main[i]);
-}
+	for (i = 0; i < number; i++)
+	{
+		if (i == number  - 1)
+	{
+		printf("%02hhx\n", p_to_main[i]);
+		break;
+	}
+		printf("%02hhx ", p_to_main[i]);
+	}
 return (0);
 }
