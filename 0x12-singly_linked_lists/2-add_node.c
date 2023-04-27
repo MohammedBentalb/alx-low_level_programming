9cc49e5bde7d564dd93898dd58ec34bd6a2a5646
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-27 09:53:29 -0700

    Task : 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,6 +10,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
+	if (head == NULL || str == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
