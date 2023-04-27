8679296c3d0e0bfebd3566847b5d409393b3ce89
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-27 09:47:48 -0700

    Task : 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * *add_node - Function that add a node at the beginning
+ * @head: pointer
+ * @str: pointer
+ * Return: Return new
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+
+	*head = new;
+return (new);
+}
