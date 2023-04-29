d4c54f668427d8bfac02507a8e8c8b75797ae9fc
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-29 16:58:14 -0700

    Task : 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,10 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
-
 	if (idx == 0)
 	{
-
 		new = malloc(sizeof(listint_t));
 		if (!new)
 		{
@@ -30,7 +28,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		add = new;
 		return (new);
 	}
-
 	while (i < idx && add != NULL)
 	{
 		i++;
