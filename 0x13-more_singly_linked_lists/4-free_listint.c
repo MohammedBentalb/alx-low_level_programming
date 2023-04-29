c9455b95a607041b71724792c2f3cbcfaa3bb076
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-04-29 09:32:25 -0700

    Task : 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - Function that frees a list
+ * @head: pointer
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *addr;
+
+	while (head->next)
+	{
+		addr = head->next;
+		free(head);
+		head = addr;
+	}
+}
