bef699d4142aef747d1c158583ead221b01a5747
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-05-02 07:57:29 -0700

    Task : 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *alt = *head;
 	unsigned int i;
 
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
