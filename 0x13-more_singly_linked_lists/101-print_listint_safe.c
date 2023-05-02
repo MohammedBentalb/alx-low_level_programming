fbeb63d6f76484425b72d3add99c3ef7bc4b5db5
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-05-02 10:27:59 -0700

    Task : 11

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Function prints a list
+ * @head: pointer
+ * Return: Return n
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int n = 0;
+	const listint_t *print;
+	const listint_t *not_n;
+
+	print = head;
+	not_n = head;
+
+	if (head == NULL)
+		exit(98);
+
+	while (not_n)
+	{
+		printf("[%p] %d\n", (void *)print, print->n);
+		n++;
+
+		if (not_n <= not_n->next)
+		{
+			printf("-> [%p] %d\n", (void *)not_n->next, not_n->next->n);
+			break;
+		}
+		print = print->next;
+		not_n = not_n->next;
+	}
+
+	if (not_n)
+	{
+		exit(98);
+	}
+
+	return (n);
+}
