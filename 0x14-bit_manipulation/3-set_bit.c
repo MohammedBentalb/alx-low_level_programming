f1c149a619727e544b346b00152f90aef5534eb7
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-05-04 10:50:06 -0700

    Task : 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - Function to replace value of a bit to 1
+ * @n: number
+ * @index: index
+ * Return: 1 || -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int res;
+
+	if (index > 63)
+		return (-1);
+
+	res = 1 << index;
+	*n |=res;
+	return (1);
+}
