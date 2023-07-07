88074f72ec566990fea1f008128b2b794a4329f9
mohammed bentalb <mohammedbentalb0@gmail.com>
2023-07-07 14:15:17 +0100

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-#include <stdlib.h>
 /**
  * hash_table_create - Funtion to creat a table
  * @size: size of the table
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,11 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -10,9 +16,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -25,8 +31,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
